3a658e2ecb33ba9aa9340cf54b2c862c2cfb018e
Shichun Yao <shichun.yao@sap.com>
2015-04-23 14:27:53 +0800

    add abstract jarLibrary

---
+++ jarLibrary/src/main/java/com/test/ysc/jar/libaray/AbstractDemo.java
@@ -0,0 +1,11 @@
+package com.test.ysc.jar.libaray;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+public class AbstractDemo {
+	@Autowired
+	protected ApplicationContext appCtx;
+	
+	
+}
---
+++ spring-web/yscWebTest/src/main/java/com/test/jar/service/DemoServiceImpl.java
@@ -0,0 +1,10 @@
+package com.test.jar.service;
+
+public class DemoServiceImpl implements DemoService{
+
+	public String getDemoResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
